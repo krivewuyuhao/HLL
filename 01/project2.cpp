37983339b164b376956031a9dce7ffb122ba03ce
Krive <1784195276@qq.com>
2023-11-26 19:39:13 +0800

    最终作业

--- 01/project2.cpp
+++ 01/project2.cpp
@@ -5,41 +5,47 @@
 #include <algorithm>
 
 using namespace std;
-
-class TeamLeader {
+/*
+*没有使用枚举来列出难度，且没有乘除法，
+*乘除法的选择也可以根据随机数的方式来随机用switch语句
+*封住函数，分文件编写，在vscode中至今不会
+*/
+//类
+class TeamLeader {                                              
 public:
     //将记录的时间放在容器里储存
+    //使用常量函数，保证外部只读取外部的数据而不进行修改
     void recordAnswerTime(double timeTaken) {
         answerTimes.push_back(timeTaken);                   
     }
 
     //计算平均用时
     double calculateAverageTime() const {
-        if (answerTimes.empty()) {
+        if (answerTimes.empty()) {               //在容器的头文件中作为布尔函数如果时间为空返回1，不为空为0，且empty比size耗时更短
             return 0.0;
         }
         double sum = 0.0;
-        for (double time : answerTimes) {
+        for (double time : answerTimes) {        //逐一把容器中的数字给time进行循环
             sum += time;
         }
-        return sum / answerTimes.size();
+        return sum / answerTimes.size();         //用size函数来确定容器中数量的多少
     }
 
     double getShortestTime() const {
         if (answerTimes.empty()) {
             return 0.0;
         }
-        return *std::min_element(answerTimes.begin(), answerTimes.end());
+        return *std::min_element(answerTimes.begin(), answerTimes.end());   //返回容器中的最小值
     }
 
     double getLongtestTime() const {
         if (answerTimes.empty()){
             return 0.0;
         }
-        return *std::max_element(answerTimes.begin(), answerTimes.end());
+        return *std::max_element(answerTimes.begin(), answerTimes.end()); //返回容器中的最大值
         
     }
-
+//设置为私有，计算时间
 private:
     std::vector<double> answerTimes;
 };
@@ -48,9 +54,10 @@ int main() {
     srand((unsigned int)time(NULL));
     int correctcount = 0;
     int wrongcount = 0;
-    vector<pair<string, int>> wronganswers; // 用于存储答错的题目和正确答案
+    //用pair来讲两组数据合成一组数据
+    vector<pair<string, int>> wronganswers;             // 用于存储答错的题目和正确答案
 
-    TeamLeader teamLeader;  // 创建 TeamLeader 对象
+    TeamLeader teamLeader;                              // 创建 TeamLeader 对象
 
     int a, b;
     int quantity;
@@ -69,22 +76,25 @@ int main() {
 
         int i = rand() % (b - a + 1) + a;
         int j = rand() % (b - a + 1) + a;
-        char symbol = rand() % 2 == 0 ? '+' : '-';  // 随机加减法
+        char symbol = rand() % 2 == 0 ? '+' : '-';  // 随机值为0，1，用三目运算符加减法   
         cout << i << symbol << j << "=" << endl;
 
         // 记录开始时间
-        time_t startTime = time(NULL);
-
+        time_t startTime = time(NULL);              
         cin >> result;
 
         // 记录结束时间
         time_t endTime = time(NULL);
-        double timeTaken = difftime(endTime, startTime);
+        double timeTaken = difftime(endTime, startTime);       //计算时间差 相当于endTime-startTime
+
+        if(symbol=='+') answer=i+j;
+        else if(symbol=='-')  answer=i-j;
 
         if (answer == result) ++correctcount;
         if (answer != result) {
             ++wrongcount;
-            wronganswers.push_back(make_pair(to_string(i) + string(1, symbol) + to_string(j), answer));
+            //将错误的题目存储到容器之中，用make_pair将其连接后存储,t0_string用于强制转化
+            wronganswers.push_back(make_pair(to_string(i) + string(1, symbol) + to_string(j), answer)); 
         }
 
         // 记录答题用时
